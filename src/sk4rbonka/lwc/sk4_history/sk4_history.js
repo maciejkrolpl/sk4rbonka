d50780deb0a8824c223bf1ec496f75a143c4adf8
Maciej Król <maciej.krol@something.xx>
2023-05-09 23:10:52 +0200

    Display history

--- src/sk4rbonka/lwc/sk4_history/sk4_history.js
+++ src/sk4rbonka/lwc/sk4_history/sk4_history.js
@@ -1,22 +1,37 @@
-import { LightningElement, track, api } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import getTransfersByChildren from '@salesforce/apex/sk4_TransferController.getTransfersByChildren';
+import getMinusTypes from '@salesforce/apex/sk4_TransferController.getMinusTypes';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class Sk4_history extends LightningElement {
+  columns = [
+    { label: 'Date', fieldName: 'dt', type: 'date-local' },
+    { label: 'Type', fieldName: 'type' },
+    { label: 'Description', fieldName: 'description' },
+    { label: 'Amount', fieldName: 'amount', type: 'currency', cellAttributes: { class: { fieldName: 'color' } } },
+    { label: 'Balance', fieldName: 'total', type: 'currency' },
+  ];
 
   @api recordId;
-  @track transfers;
-
-  async connectedCallback() {
-    const transfers = await getTransfersByChildren({childrenId: this.recordId});
-    console.log("🚀 ~ connectedCallback ~ transfers:", JSON.parse(JSON.stringify(transfers)))
-    this.transfers = transfers;
-    this.countBalances();
-  }
-
-  countBalances() {
-
-    // const transfers = this.transfers.map(transfer => {
-    //   const balanceAf
-    // })
+  @wire(getMinusTypes) minusTypes;
+  @wire(getTransfersByChildren, { childId: '$recordId' }) wireResult;
+  get transfers() {
+      if (!this.minusTypes) {
+      return null;
+    }
+    if (this.wireResult.error) {
+      const toast = new ShowToastEvent({
+        title: 'Error',
+        variant: 'error',
+        message: this.wireResult.error.body.message
+      });
+      this.dispatchEvent(toast);
+      return null;
+    }
+    
+    return this.wireResult.data?.map(item => ({
+      ...item,
+      color: (this.minusTypes.data.includes(item.type)) ? 'slds-text-color_error' : ''
+    }));
   }
 }
