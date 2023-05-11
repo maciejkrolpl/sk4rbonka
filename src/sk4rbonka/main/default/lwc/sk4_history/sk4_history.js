519e34911de7456f0df3f556b0059ec471bc5b67
Maciej Król <maciej.krol@something.xx>
2023-05-11 11:57:01 +0200

    formatted src

--- src/sk4rbonka/main/default/lwc/sk4_history/sk4_history.js
+++ src/sk4rbonka/main/default/lwc/sk4_history/sk4_history.js
@@ -1,58 +1,58 @@
-import { LightningElement, wire, api, track } from "lwc";
-import getTransfersByChildren from "@salesforce/apex/sk4_TransferController.getTransfersByChildren";
-import getMinusTypes from "@salesforce/apex/sk4_TransferController.getMinusTypes";
-import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { LightningElement, wire, api, track } from 'lwc'
+import getTransfersByChildren from '@salesforce/apex/sk4_TransferController.getTransfersByChildren'
+import getMinusTypes from '@salesforce/apex/sk4_TransferController.getMinusTypes'
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 
 export default class Sk4_history extends LightningElement {
-  columns = [
-    { label: "Date", fieldName: "dt", type: "date-local" },
-    { label: "Type", fieldName: "type" },
-    { label: "Description", fieldName: "description" },
-    {
-      label: "Amount",
-      fieldName: "amount",
-      type: "currency",
-      cellAttributes: { class: { fieldName: "color" } }
-    },
-    { label: "Balance", fieldName: "total", type: "currency" }
-  ];
+    columns = [
+        { label: 'Date', fieldName: 'dt', type: 'date-local' },
+        { label: 'Type', fieldName: 'type' },
+        { label: 'Description', fieldName: 'description' },
+        {
+            label: 'Amount',
+            fieldName: 'amount',
+            type: 'currency',
+            cellAttributes: { class: { fieldName: 'color' } },
+        },
+        { label: 'Balance', fieldName: 'total', type: 'currency' },
+    ]
 
-  @api recordId;
-  @track transfers;
-  @track minusTypes;
+    @api recordId
+    @track transfers
+    @track minusTypes
 
-  displayErrorToast(message) {
-    const toast = new ShowToastEvent({
-      title: "Error",
-      variant: "error",
-      message
-    });
-    this.dispatchEvent(toast);
-  }
-
-  @wire(getMinusTypes) wireMinusTypes({ error, data }) {
-    if (data) {
-      this.minusTypes = data;
-    } else if (error) {
-      this.minusTypes = undefined;
-      this.displayErrorToast(error.body.message);
+    displayErrorToast(message) {
+        const toast = new ShowToastEvent({
+            title: 'Error',
+            variant: 'error',
+            message,
+        })
+        this.dispatchEvent(toast)
     }
-  }
 
-  @wire(getTransfersByChildren, { childId: "$recordId" }) wireTransfers({
-    error,
-    data
-  }) {
-    if (data) {
-      this.transfers = data?.map((item) => ({
-        ...item,
-        color: this.minusTypes.includes(item.type)
-          ? "slds-text-color_error"
-          : ""
-      }));
-    } else if (error) {
-      this.transfers = undefined;
-      this.displayErrorToast(error.body.message);
+    @wire(getMinusTypes) wireMinusTypes({ error, data }) {
+        if (data) {
+            this.minusTypes = data
+        } else if (error) {
+            this.minusTypes = undefined
+            this.displayErrorToast(error.body.message)
+        }
     }
-  }
+
+    @wire(getTransfersByChildren, { childId: '$recordId' }) wireTransfers({
+        error,
+        data,
+    }) {
+        if (data) {
+            this.transfers = data?.map((item) => ({
+                ...item,
+                color: this.minusTypes.includes(item.type)
+                    ? 'slds-text-color_error'
+                    : '',
+            }))
+        } else if (error) {
+            this.transfers = undefined
+            this.displayErrorToast(error.body.message)
+        }
+    }
 }
--- src/sk4rbonka/main/default/lwc/sk4_wholesale/sk4_wholesale.js
+++ src/sk4rbonka/main/default/lwc/sk4_wholesale/sk4_wholesale.js
@@ -1,113 +1,111 @@
-import { LightningElement,  track } from 'lwc';
-import getAllChildren from '@salesforce/apex/sk4_Wholesale.getAllChildren';
-import saveNewTransfers from '@salesforce/apex/sk4_Wholesale.saveNewTransfers';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-
-
+import { LightningElement, track } from 'lwc'
+import getAllChildren from '@salesforce/apex/sk4_Wholesale.getAllChildren'
+import saveNewTransfers from '@salesforce/apex/sk4_Wholesale.saveNewTransfers'
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 
 export default class Sk4_wholesale extends LightningElement {
-  @track children;
-  @track error;
+    @track children
+    @track error
 
-  connectedCallback() {
-    this.getAllChildren();
-  }
-
-  getAllChildren() {
-    this.children = undefined;
-    this.error = undefined;
-    getAllChildren()
-      .then(children => {
-        this.children = children.map(child => ({ ...child, give: 0, save: 0 }));
-        console.log( JSON.parse(JSON.stringify(this.children)) )
-      })
-      .catch(error => this.error = error);
-  }
-
-  handleChange(event) {
-    const amount = event.target.value;
-    const input = event.target.dataset.input;
-    const name = event.target.dataset.name;
-    const children = this.children.map(child => {
-      if (child.fullName === name) {
-        child[input] = +amount;
-      }
-      return child;
-    })
-    this.children = children;
-    
-  }
-
-  popUpEvent(variant, message) {
-    const toast = new ShowToastEvent({
-      title: variant.toUpperCase(),
-      variant,
-      message
-    });
-    this.dispatchEvent(toast);
-  }
-
-  clearInputs() {
-    this.children.forEach(child => {
-      child.save = 0;
-      child.give = 0;
-    })
-    this.template.querySelectorAll('.input-save')
-      .forEach(input => {
-        input.setCustomValidity('');
-        input.reportValidity();
-      })
-    
-  }
-
-  handleSave() {
-    console.log( JSON.parse(JSON.stringify(this.children)) )
-    if (!this.checkValidity()) {
-      return;
+    connectedCallback() {
+        this.getAllChildren()
     }
 
-    const children = this.children.filter(child => child.give > 0);
-
-    if (!children.length) {
-      return;
+    getAllChildren() {
+        this.children = undefined
+        this.error = undefined
+        getAllChildren()
+            .then((children) => {
+                this.children = children.map((child) => ({
+                    ...child,
+                    give: 0,
+                    save: 0,
+                }))
+                console.log(JSON.parse(JSON.stringify(this.children)))
+            })
+            .catch((error) => (this.error = error))
     }
 
-    saveNewTransfers({ children })
-      .then(() => {
-        this.popUpEvent('success', 'Operation completed successfully.');
-        this.clearInputs();
-        this.getAllChildren();
-      })
-      .catch(error => {
-        this.popUpEvent('error', error.body.message);
-        console.error(error);
-      })
-  }
+    handleChange(event) {
+        const amount = event.target.value
+        const input = event.target.dataset.input
+        const name = event.target.dataset.name
+        const children = this.children.map((child) => {
+            if (child.fullName === name) {
+                child[input] = +amount
+            }
+            return child
+        })
+        this.children = children
+    }
 
-  checkValidity() {
-    let isValid = true;
-    this.children.forEach(child => {
-      const input = this.template.querySelector(`.${child.id} .input-save`);
-      if (child.save > child.give) {
-        input.setCustomValidity(' ');
-        this.popUpEvent('error', 'Cannot save more than is paid')
-        isValid = false;
-      } else {
-        input.setCustomValidity('');
-      }
-      input.reportValidity();
+    popUpEvent(variant, message) {
+        const toast = new ShowToastEvent({
+            title: variant.toUpperCase(),
+            variant,
+            message,
+        })
+        this.dispatchEvent(toast)
+    }
 
-    })
-    return isValid;
+    clearInputs() {
+        this.children.forEach((child) => {
+            child.save = 0
+            child.give = 0
+        })
+        this.template.querySelectorAll('.input-save').forEach((input) => {
+            input.setCustomValidity('')
+            input.reportValidity()
+        })
+    }
 
-  }
+    handleSave() {
+        console.log(JSON.parse(JSON.stringify(this.children)))
+        if (!this.checkValidity()) {
+            return
+        }
 
-  defaultAmount() {
-    this.children.forEach(child => {
-      if (child.defaultAmount) {
-        child.give = +child.defaultAmount;
-      }
-    })
-  }
+        const children = this.children.filter((child) => child.give > 0)
 
+        if (!children.length) {
+            return
+        }
+
+        saveNewTransfers({ children })
+            .then(() => {
+                this.popUpEvent('success', 'Operation completed successfully.')
+                this.clearInputs()
+                this.getAllChildren()
+            })
+            .catch((error) => {
+                this.popUpEvent('error', error.body.message)
+                console.error(error)
+            })
+    }
+
+    checkValidity() {
+        let isValid = true
+        this.children.forEach((child) => {
+            const input = this.template.querySelector(
+                `.${child.id} .input-save`
+            )
+            if (child.save > child.give) {
+                input.setCustomValidity(' ')
+                this.popUpEvent('error', 'Cannot save more than is paid')
+                isValid = false
+            } else {
+                input.setCustomValidity('')
+            }
+            input.reportValidity()
+        })
+        return isValid
+    }
+
+    defaultAmount() {
+        this.children.forEach((child) => {
+            if (child.defaultAmount) {
+                child.give = +child.defaultAmount
+            }
+        })
+    }
 }
